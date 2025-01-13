24974e99b41ca9b88808efcc37f3c935ff8bbbc9
reece.mi+ezl@pixelforce.com.au <reece.mi+ezl@pixelforce.com.au>
2025-01-13 01:02:47 +0000

    dbt test

---
+++ models/example/vw_failed_search_stat.sql
@@ -0,0 +1,25 @@
+{{ config(
+    materialized='view',
+    database='ezlicence-1506735963116'
+) }}
+
+with fail_record as (
+    select *
+    from {{ source('production', 'public_public_search_records') }}
+    where result_count = 0 
+    and date(created_at) > '2024-09-15'
+    order by created_at desc
+)
+
+select 
+    fail_record.suburb,
+    event_tracking_type,
+    transmission, 
+    date(created_at) as date,
+    geo_layers.State,
+    geo_layers.MicroRegion,
+    geo_layers.MacroRegion
+from fail_record
+left join {{ source('dimensions', 'au_vw_geo_layers') }} geo_layers 
+    on fail_record.suburb_polygon_id = geo_layers.suburb_polygon_id
+where event_tracking_type is not null
