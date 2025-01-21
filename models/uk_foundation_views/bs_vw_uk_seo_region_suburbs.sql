ed87b89c01ddc1044062dfd3bf73caec5637ea90
EzLicence Accounts <accounts@ezlicence.com.au>
2025-01-21 07:05:51 +0000

    foundation view uk

--- models/uk_foundation_views/bs_vw_uk_seo_region_suburbs.sql
+++ models/uk_foundation_views/bs_vw_uk_seo_region_suburbs.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized='view',
     database='ezlicence-1506735963116',
-    schema = 'au_foundation_view'
+    schema = 'uk_foundation_view'
 ) }}
 
 SELECT
@@ -22,4 +22,4 @@ SELECT
    -- Audit Fields
    CURRENT_TIMESTAMP() AS foundation_created_at
 
-FROM `ezlicence-1506735963116.au_production_2_bq.public_seo_region_suburbs`
+FROM `ezlicence-1506735963116.uk_production_2_bq.seo_region_suburbs`
